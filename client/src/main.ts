ede88e2eafac88ad12a3289edb0e2de7b5b1a709
Lino Meyer <lino.meyer@stud.hslu.ch>
2026-02-06 15:47:54 +0100

    setup auth0

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,22 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {appConfig} from './app/app.config';
+import {AppComponent} from './app/app.component';
+import {mergeApplicationConfig} from '@angular/core';
+import {provideAuth0} from '@auth0/auth0-angular';
+import {environment} from './environments/environment';
+
+
+const auth0Config = mergeApplicationConfig(appConfig, {
+  providers: [
+    provideAuth0({
+      domain: environment.auth0.domain,
+      clientId: environment.auth0.clientId,
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    })
+  ]
+});
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
