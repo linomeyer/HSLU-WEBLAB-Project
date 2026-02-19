37928936ed26b609ab7a2504fa524855eb1b8cf6
Lino Meyer <lino.meyer@stud.hslu.ch>
2026-02-19 17:21:58 +0100

    set changedAt on update

--- server/src/technologies/technology.service.ts
+++ server/src/technologies/technology.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Technology } from './technology.schema';
 import { Model } from 'mongoose';
@@ -17,17 +17,31 @@ export class TechnologyService {
     return this.technologyModel.find().exec();
   }
 
-  async findOne(id: string): Promise<Technology | null> {
-    return this.technologyModel.findById(id).exec();
+  async findOne(id: string): Promise<Technology> {
+    const tech = await this.technologyModel.findById(id).exec();
+    if (!tech) {
+      throw new NotFoundException(`Technology with ID ${id} not found`);
+    }
+    return tech;
   }
 
-  async update(id: string, technology: Technology): Promise<Technology | null> {
-    return this.technologyModel
+  async update(id: string, technology: Technology): Promise<Technology> {
+    technology.changedAt = new Date();
+    const updatedTech = await this.technologyModel
       .findByIdAndUpdate(id, technology, { new: true })
       .exec();
+
+    if (!updatedTech) {
+      throw new NotFoundException(`Technology with ID ${id} not found`);
+    }
+    return updatedTech;
   }
 
-  async delete(id: string): Promise<Technology | null> {
-    return this.technologyModel.findByIdAndDelete(id).exec();
+  async delete(id: string): Promise<Technology> {
+    const deletedTech = await this.technologyModel.findByIdAndDelete(id).exec();
+    if (!deletedTech) {
+      throw new NotFoundException(`Technology with ID ${id} not found`);
+    }
+    return deletedTech;
   }
 }
