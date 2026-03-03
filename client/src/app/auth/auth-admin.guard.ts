1e84c2e2dd168e597e6057decba06e6619d3ef96
Lino Meyer <lino.meyer@stud.hslu.ch>
2026-03-03 17:26:23 +0100

    move roles_claim to environment.ts

--- client/src/app/auth/auth-admin.guard.ts
+++ client/src/app/auth/auth-admin.guard.ts
@@ -1,12 +1,11 @@
 import {inject} from '@angular/core';
-import {Router} from '@angular/router';
 import {AuthService} from '@auth0/auth0-angular';
 import {filter, map, switchMap, take} from 'rxjs/operators';
 import {from} from 'rxjs';
+import {environment} from '../../environments/environment';
 
 export const authAdminGuard = () => {
   const auth = inject(AuthService);
-  const router = inject(Router);
 
   return auth.isAuthenticated$.pipe( // if authenticated, check roles via JWT token
     filter((isAuthenticated) => isAuthenticated !== null),
@@ -15,7 +14,7 @@ export const authAdminGuard = () => {
     map((token) => {
       // atob == base64 decode of JWT token
       const payload = JSON.parse(atob(token.split('.')[1]));
-      const roles: string[] = payload['https://technology-radar.com/roles'] || [];
+      const roles: string[] = payload[environment.auth0.roles_claim] || [];
       return roles.includes('admin');
     })
   );
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,5 +4,6 @@ export const environment = {
     domain: 'dev-82m7uo7ptlal3yfw.us.auth0.com',
     clientId: '1UjtvXODOR5kOzQnN68u94IjcRWm4g1Y',
     audience: 'https://technology-radar-api',
+    roles_claim: 'https://technology-radar.com/roles'
   }
 };
