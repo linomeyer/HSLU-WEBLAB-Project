f220ebeae15c2714cef9cbfc93ed7a52b55093b6
Lino Meyer <lino.meyer@stud.hslu.ch>
2026-03-04 20:47:23 +0100

    set up docker image with data init script to replace atlas mongodb

--- server/src/main.ts
+++ server/src/main.ts
@@ -21,6 +21,14 @@ async function bootstrap() {
     .setTitle('Technology-Radar')
     .setDescription('Technology-Radar API')
     .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT token',
+      in: 'header',
+    })
     .build();
 
   const documentFactory = () =>
